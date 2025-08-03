1ccf8f7f2e5815499cc2b36e08d3773e600e3c26
anil <anilpadghane12@gmail.com>
2025-08-03 16:03:33 +0530

    Update dependencies and fix PostCSS config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,7 @@
-// postcss.config.cjs
+// For CommonJS (postcss.config.js OR postcss.config.cjs)
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
